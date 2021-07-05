be9ed815573176387dcc7e2a0cd8f0073e2e2ebc
nmerolle <nmerolle11@hotmail.com>
2021-07-04 22:32:24 -0400

    renamed attributes in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :coupons
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete '/logout' => "sessions#destroy"
-  #post '/login' 'sessions#new'
+  resources :coupons
   resources :stores
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_231632) do
+ActiveRecord::Schema.define(version: 2021_07_05_005358) do
 
   create_table "coupons", force: :cascade do |t|
     t.string "product_name"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_231632) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
