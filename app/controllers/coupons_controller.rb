ec8a22ecb26ff0d8185f785966a93eccff0f5c31
nmerolle <nmerolle11@hotmail.com>
2021-07-11 00:50:23 -0400

    added methods to coupon controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,49 @@ class CouponsController < ApplicationController
   def index
     @coupons = Coupon.all
   end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = current_user.coupons.build(coupon_params)
+    if @coupon.save
+      redirect_to coupons_path
+    else
+      render :new
+    end  
+  end
+
+  def edit 
+    find_coupon
+  end
+
+  def update
+    find_coupon
+    @copuon.update(coupn_params)
+    if @coupon.valid?
+      redirect_to coupons_path
+    else
+      render :edit 
+    end
+  end
+
+  def destroy
+    find_coupon
+    @coupon.destroy
+    redirect_to coupons_path
+  end
+
+
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:product_name, :expiration_date, :value)
+  end
+  
+  def find_coupon
+    @coupon = Coupon.find(params[:id])
+  end
 end
