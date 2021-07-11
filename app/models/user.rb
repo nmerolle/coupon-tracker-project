43bac19fb5b450ea51c8f48ab7182c4a2888d7d3
nmerolle <nmerolle11@hotmail.com>
2021-07-11 05:58:36 -0400

    added model  associationsto user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_many :coupons
+  has_many :stores, through: :coupons
   has_secure_password
   validates :username, :email,  presence: true
   validates :email, uniqueness: true
-  has_many :coupons
+ 
 end
