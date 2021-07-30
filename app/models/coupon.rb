bc72d6f87a78ae38347c3cf3e1f2bdee7dfb015d
nmerolle <nmerolle11@hotmail.com>
2021-07-29 23:46:43 -0400

    created scope method

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,6 +3,7 @@ class Coupon < ApplicationRecord
   belongs_to :store
   validates :product_name, :expiration_date, :value, presence: true
   accepts_nested_attributes_for :store, reject_if: :all_blank
+  scope :expired_coupon, -> {where("expiration_date < ?", Date.today)}
 
   def store_name
      self.store.name if self.store
